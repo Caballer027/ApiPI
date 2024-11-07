5a62e1115fb189bf1432dbaab76e92b980c28502
Caballer027 <leocaballerohuamani27@gmail.com>
2024-11-07 02:20:47 -0500

    DATABASES

--- mathplay_admin/settings.py
+++ mathplay_admin/settings.py
@@ -90,12 +90,7 @@ WSGI_APPLICATION = 'mathplay_admin.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600)
-        
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 
